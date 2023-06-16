f3cf4d48e13be142f157d998d4d809b35df6b672
Call Vin <kanggara75@gmail.com>
2023-06-16 22:34:25 +0700

    update style css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  darkMode: 'class',
-=======
   darkMode: "class",
->>>>>>> 72c7bbfa8d4cd9f71b69e190b942c0959c24902f
   content: ["./**/*.{html,js}"],
   theme: {
     container: {
       center: true,
-<<<<<<< HEAD
-      padding: '16px',
-    },
-    extend: {
-      colors: {
-        primary: '#14b8a6',
-        dark: '#0f172a',
-        ocean: '#FEE2E2',
-        yellow: '#FDE68A'
-      },
-      screens: {
-        '2xl': '1320px'
-      }
-=======
       padding: "16px",
     },
     extend: {
@@ -35,8 +17,7 @@ module.exports = {
       screens: {
         "2xl": "1320px",
       },
->>>>>>> 72c7bbfa8d4cd9f71b69e190b942c0959c24902f
     },
   },
   plugins: [],
-}
+};
