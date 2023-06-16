4b5cef95e05f90d83764612b150367abe4f869da
MistamiAlfariski <ibuslenovo6@gmail.com>
2023-06-16 15:29:19 +0700

    MAPS

---
+++ assets/js/scripts.js
@@ -0,0 +1,70 @@
+// Navbar Fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
+  
+    if (window.pageYOffset > fixedNav) {
+      header.classList.add('navbar-fixed');
+      toTop.classList.remove('hidden');
+      toTop.classList.add('flex');
+    } else {
+      header.classList.remove('navbar-fixed');
+      toTop.classList.remove('flex');
+      toTop.classList.add('hidden');
+    }
+  };
+  
+  // Hamburger
+  const hamburger = document.querySelector('#hamburger');
+  const navMenu = document.querySelector('#nav-menu');
+  
+  hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+  });
+  
+  // Klik di luar hamburger
+  window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+      hamburger.classList.remove('hamburger-active');
+      navMenu.classList.add('hidden');
+    }
+  });
+  
+  // Darkmode toggle
+  const darkToggle = document.querySelector('#dark-toggle');
+  const html = document.querySelector('html');
+  
+  darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+      html.classList.add('dark');
+      localStorage.theme = 'dark';
+    } else {
+      html.classList.remove('dark');
+      localStorage.theme = 'light';
+    }
+  });
+  
+  // pindahkan posisi toggle sesuai mode
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
+
+  function initMap() {
+    // Menampilkan peta pada div dengan id 'map'
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: {lat: -6.1754, lng: 106.8272}
+    });
+    
+    // Menambahkan marker pada lokasi yang ditentukan
+    var marker = new google.maps.Marker({
+      position: {lat: -6.1754, lng: 106.8272},
+      map: map,
+      title: 'Lokasi Saya'
+    });
+  }
+  
