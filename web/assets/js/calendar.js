a2c1538c1415e206262b5d187072a480622b401a
Call Vin <kanggara75@gmail.com>
2023-06-26 15:40:57 +0700

    fix: calender when screen size change

--- web/assets/js/calendar.js
+++ web/assets/js/calendar.js
@@ -73,43 +73,45 @@ readTextFile("./assets/date.json", function (text) {
 		});
 	}
 
-	if (screen.width > 768) {
-		let date = 0;
-		let start = idx[0] + 1;
-		let end = idx[0] + 7;
-		for (let week = 1; week <= (idx[1] + 1) / 7; week++) {
-			const newRow1 = document.createElement("tr");
-			for (i = start; i <= end; i++) {
+	date = 0;
+	start = idx[0] + 1;
+	end = idx[0] + 7;
+	for (let week = 1; week <= (idx[1] + 1) / 7; week++) {
+		const newRow1 = document.createElement("tr");
+		for (i = start; i <= end; i++) {
+			if (i < dates.length) {
 				date = dates[i].date;
 				avail = dates[i].status == "available" ? "text-black" : "text-gray-500";
 				pointer = dates[i].status == "available" ? "cursor-pointer" : "cursor-not-allowed";
-				const tdDate = document.createElement("td");
-				if (date != 0) {
-					const div = document.createElement("div");
-					div.classList.add("flex", "w-full", pointer, "items-center", "justify-center", "rounded-full", "p-2", "font-medium", avail);
-					if (dates[i].status == "available") {
-						div.classList.add("hover:h-8");
-						div.classList.add("hover:w-8");
-						div.classList.add("hover:bg-indigo-500");
-						div.classList.add("hover:bg-indigo-500");
-						div.classList.add("hover:text-white");
-					}
-					const p = document.createElement("p");
-					p.setAttribute("aria-label", dates[i].label);
-					isBlock = dates[i].isBlock ? p.classList.add("text-base", "line-through") : p.classList.add("text-base");
-					p.textContent = date;
-					div.appendChild(p);
-					tdDate.appendChild(div);
-				}
-
-				newRow1.appendChild(tdDate);
 			}
-			start += 7;
-			end += 7;
-			window.addEventListener("load", function () {
-				const target = document.getElementById("calendar2");
-				target.appendChild(newRow1);
-			});
+			const tdDate = document.createElement("td");
+			if (date != 0) {
+				const div = document.createElement("div");
+				div.classList.add("flex", "w-full", pointer, "items-center", "justify-center", "rounded-full", "p-2", "font-medium", avail);
+				if (dates[i].status == "available") {
+					div.classList.add("hover:h-8");
+					div.classList.add("hover:w-8");
+					div.classList.add("hover:bg-indigo-500");
+					div.classList.add("hover:bg-indigo-500");
+
+					div.classList.add("hover:text-white");
+				}
+				const p = document.createElement("p");
+				p.setAttribute("aria-label", dates[i].label);
+				isBlock = dates[i].isBlock ? p.classList.add("text-base", "line-through") : p.classList.add("text-base");
+				p.textContent = date;
+				div.appendChild(p);
+				tdDate.appendChild(div);
+			}
+
+			newRow1.appendChild(tdDate);
 		}
+		start += 7;
+		end += 7;
+
+		const target = document.getElementById("calendar2");
+		window.addEventListener("load", function () {
+			target.appendChild(newRow1);
+		});
 	}
 });
